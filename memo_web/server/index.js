2942b16bd2e441c7fa235dca44f928e86486c71f
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2022-09-23 00:28:33 +0900

    User.js -> methods update
    methods update 및 추가 download

--- memo_web/server/index.js
+++ memo_web/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const {auth} = require("./middleware/auth");
 const {User} = require("./models/User");
 const config = require('./config/key');
 
@@ -8,6 +10,7 @@ const config = require('./config/key');
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -15,14 +18,11 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.post('/register', (req, res) => {
-    //회원 강비 할때 필요한 정보들을 client에서 가져오면
-    //그것들을 데이터 베이스에 넣어준다
-  
     const user = new User(req.body)
   
     user.save((err,userInfo) => {
